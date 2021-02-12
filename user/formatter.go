2def6c5bf2228df0c2d599bb2f8da085d4a06827
mashudi rohmat <mashudirohmat.mr@gmail.com>
2021-02-12 22:36:52 +0700

    init project, Register user endpoint

---
+++ user/formatter.go
@@ -0,0 +1,19 @@
+package user
+
+type UserFormatter struct {
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	Occupation string `json:"occupation"`
+	Email      string `json:"email"`
+	Token      string `json:"token"`
+}
+
+func FormatUser(user User, token string) UserFormatter {
+	formatter := UserFormatter{
+		ID:    user.ID,
+		Name:  user.Name,
+		Email: user.Email,
+		Token: token,
+	}
+	return formatter
+}
